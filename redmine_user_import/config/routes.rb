be1c16fa80120aed858f462166359bfee2f1cdd7
nobby256 <helloworld_1024@yahoo.co.jp>
2015-01-25 17:02:13 +0900

    追加

--- redmine_user_import/config/routes.rb
+++
@@ -1,7 +0,0 @@
-RedmineApp::Application.routes.draw do
-  scope 'user_import' do
-    get 'index', :controller => 'user_import', :action => 'index'
-    post 'match', :controller => 'user_import', :action => 'match'
-    post 'result', :controller => 'user_import', :action => 'result'
-  end
-end
