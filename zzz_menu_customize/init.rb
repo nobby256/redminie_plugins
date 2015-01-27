4ee5bae8ad241dd328d3064b02079d5b603fa8f4
nhashimoto <noboru_hashimoto@scsk-nearshore.co.jp>
2015-01-28 08:55:36 +0900

    プロジェクトメニューを整頓

--- zzz_menu_customize/init.rb
+++ zzz_menu_customize/init.rb
@@ -6,9 +6,13 @@ Redmine::Plugin.register :zzz_menu_customize do
 
   Redmine::MenuManager.map :project_menu do |menu|
     menu.delete :roadmap
-#    menu.delete :work_time
+    menu.delete :overview
+    menu.delete :activity
     
 #    menu.push :zzz_menu_customize, { :controller => 'parking_lot_chart', :action => 'index' }, :parent => :gantt
+    menu.delete :version_burndown_charts
+    menu.push :version_burndown_charts, { :controller => 'version_burndown_charts', :action => 'index' }, :caption => :version_burndown_charts, :param => :project_id, :after => :parking_lot_chart
+    menu.delete :gantt
+    menu.push :gantt, { :controller => 'gantts', :action => 'show' }, :param => :project_id, :caption => :label_gantt, :after => :parking_lot_chart
   end
-
 end
