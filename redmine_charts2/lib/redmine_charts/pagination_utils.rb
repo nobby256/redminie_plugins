df85d7debc04c52be5a18a0d6e6473b2a0a3bd70
nhashimoto <noboru_hashimoto@scsk-nearshore.co.jp>
2015-01-30 22:51:48 +0900

    更新

--- redmine_charts2/lib/redmine_charts/pagination_utils.rb
+++ redmine_charts2/lib/redmine_charts/pagination_utils.rb
@@ -4,7 +4,7 @@ module RedmineCharts
     def self.from_params(params)
       {
         :page => params[:page].blank? ? 1 : Integer(params[:page]),
-        :per_page => params[:per_page].blank? ? 10 : Integer(params[:per_page]),
+        :per_page => params[:per_page].blank? ? 50 : Integer(params[:per_page]),
       }
     end
     
