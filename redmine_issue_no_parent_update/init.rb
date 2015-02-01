7dec15b8776e81eca8e0882e2b60bd6b42fea0f4
nobby256 <helloworld_1024@yahoo.co.jp>
2015-02-01 16:36:57 +0900

    大幅追加

--- redmine_issue_no_parent_update/init.rb
+++ redmine_issue_no_parent_update/init.rb
@@ -1,10 +1,21 @@
 require_dependency 'issue_no_parent_update_patch'
 require_dependency 'issue_no_parent_update_listener'
 require_dependency 'issue_remaining_hours_listener'
+require_dependency 'issue_patch'
+require_dependency 'issue_query_patch'
+require_dependency 'project_patch'
+require_dependency 'queries_helper_patch'
 
 Rails.configuration.to_prepare do
   require_dependency 'issue'
+  require_dependency 'issue_sub_category'
+  require_dependency 'project'
+
   Issue.send(:include, IssueNoParentUpdatePatch)
+  Issue.send(:include, IssueNoParentUpdate::IssuePatch)
+  IssueQuery.send(:include, IssueNoParentUpdate::IssueQueryPatch)
+  QueriesHelper.send(:include, IssueNoParentUpdate::QueriesHelperPatch)
+  Project.send(:include, IssueNoParentUpdate::ProjectPatch)
 end
 
 Redmine::Plugin.register :redmine_issue_no_parent_update do
